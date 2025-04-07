84c40a626ba1004e62372177ff9257aebf3281d0
MicheleDioli <micheledioli.dm@gmail.com>
2025-04-07 21:22:14 +0200

    amburger

---
+++ src/js/mburger.js
@@ -0,0 +1,12 @@
+    function apriMenu() {
+      const menuHamberger = document.querySelector('.nascondi')
+      menuHamberger.classList.remove("nascondi")
+      menuHamberger.classList.add("menuHamberger")
+    }
+
+    function chiudiMenu(){
+      const menuHamberger = document.querySelector('.menuHamberger')
+      menuHamberger.classList.remove("menuHamberger")
+      menuHamberger.classList.add("nascondi")
+    }
+
