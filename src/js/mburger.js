345555d18eeedd7cfb55d55d26496588aba9f19a
MicheleDioli <micheledioli.dm@gmail.com>
2025-05-13 21:03:19 +0200

    jsMenu

--- src/js/mburger.js
+++ src/js/mburger.js
@@ -1,11 +1,17 @@
  function apriMenu() {
       const menuHamberger = document.querySelector('.nascondi')
+      const overlay = document.querySelector('.overlayNascondi')
+      overlay.classList.remove("overlayNascondi")
+      overlay.classList.add("overlay")
       menuHamberger.classList.remove("nascondi")
       menuHamberger.classList.add("menuHamberger")
     }
 
     function chiudiMenu(){
       const menuHamberger = document.querySelector('.menuHamberger')
+      const overlay = document.querySelector('.overlay')
+      overlay.classList.remove("overlay")
+      overlay.classList.add("overlayNascondi")
       menuHamberger.classList.remove("menuHamberger")
       menuHamberger.classList.add("nascondi")
     }
