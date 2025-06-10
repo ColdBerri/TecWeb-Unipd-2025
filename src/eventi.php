85a160c0ac64a29b78e16f91d173bf6519ac647f
MicheleDioli <micheledioli.dm@gmail.com>
2025-06-10 17:37:10 +0200

    calendario

--- src/eventi.php
+++ src/eventi.php
@@ -3,7 +3,7 @@ require_once "dbconnections.php";
 require_once "template.php";
 use DB\DBAccess;
 
-$paginaHTML = new Template("","","html/eventi.html");
+$paginaHTML = new Template("", "", "html/eventi.html");
 
 $month = date('n');
 $year = date('Y');
@@ -11,18 +11,18 @@ $year = date('Y');
 $db = new DBAccess();
 $eventDays = [];
 
-$timestamp = $_SERVER[ 'REQUEST_TIME' ];
+$timestamp = $_SERVER['REQUEST_TIME'];
 
 $anno = "2025";
 
-$oggiG = date('d',$timestamp);
-$oggiM = date('m',$timestamp);
+$oggiG = date('d', $timestamp);
+$oggiM = date('m', $timestamp);
 
 $calendarioHTML = "";
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $mese = ($_POST['mese']);
-    $anno = ($_POST['anno']);;
+    $anno = ($_POST['anno']);
 } else {
     $mese = $oggiM;
 }
@@ -33,20 +33,21 @@ $connessioneOK = $connessione->openDBConnection();
 $eventi = "";
 $anni = "";
 
-if(!$connessioneOK){
-    $eventi = $connessione->getEventiMese($mese,$anno);
-    $connessione->closeConnection();    
+if (!$connessioneOK) {
+    $eventi = $connessione->getEventiMese($mese, $anno);
+    $connessione->closeConnection();
 }
 
 $giorniEvento = [];
 
-if(!empty($eventi)){
-    foreach($eventi as $ev){
+if (!empty($eventi)) {
+    foreach ($eventi as $ev) {
         $giorniEvento[] = intval(date('j', strtotime($ev['data_inizio_evento'])));
     }
 }
 
 $giorniSettimana = ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom'];
+$giorniSettimanaC = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'];
 
 $primoGiorno = mktime(0, 0, 0, $mese, 1, $anno);
 $indicePrimoGiorno = date('N', $primoGiorno);
@@ -60,59 +61,58 @@ $indiceUltimoGiorno = date('N', $ultimoGiorno);
 $giorniFinaliVuoti = 7 - $indiceUltimoGiorno;
 
 $calendarioHTML .= "<thead>";
-$calendarioHTML .= "<tr class='mese_selezionato'><th colspan='7' lang='en'>" . date('F Y', $primoGiorno) . "</th></tr>";
-$tmp = false;
+$calendarioHTML .= "<div><tr class='mese_selezionato'><th colspan='7' lang='en'>" . date('F Y', $primoGiorno) . "</th></tr></div>";
 
-if(date('m', $primoGiorno) === $oggiM){
+$tmp = false;
+if (date('m', $primoGiorno) === $oggiM) {
     $tmp = true;
 }
 
-
-$calendarioHTML .= "<tr>";
-
+$calendarioHTML .= "<tr class='listaGiorniCalendario'>";
 foreach ($giorniSettimana as $giorno) {
-    $calendarioHTML .="<th scope='col'>$giorno</th>";
+    $calendarioHTML .= "<th scope='col'>$giorno</th>";
 }
 $calendarioHTML .= "</tr></thead><tbody><tr>";
 
+$indiceGiornoSettimana = 0;
+
+// Giorni vuoti iniziali
 for ($i = $giorniInizialiVuoti; $i > 0; $i--) {
     $giorno = $giorniDelMesePrecedente - $i + 1;
-    $calendarioHTML .= "<td class='giornoFuori'>$giorno</td>";
+    $giornoSettimanaC = $giorniSettimanaC[$indiceGiornoSettimana % 7];
+    $calendarioHTML .= "<td class='giornoFuori' data-title='$giornoSettimanaC'>$giorno</td>";
+    $indiceGiornoSettimana++;
 }
 
 $giornoCorrente = 1;
 $eventoStampato = false;
 
 while ($giornoCorrente <= $numeroGiorni) {
-
     $eventoStampato = false;
+    $giornoSettimanaC = $giorniSettimanaC[$indiceGiornoSettimana % 7];
 
-    if(!(empty($eventi))){
-
+    if (!empty($eventi)) {
         foreach ($eventi as $ev) {
             $giornoEvento = intval(date('j', strtotime($ev['data_inizio_evento'])));
-            
             if ($giornoEvento === $giornoCorrente && !$eventoStampato) {
                 $nomeEvento = htmlspecialchars($ev['nome_evento']);
                 $eventoLink = urldecode($ev['nome_evento']);
 
-                if(!($tmp && $giornoCorrente == $oggiG)){
-                    $calendarioHTML .= "<td><span class='eventoPresente'>$giornoCorrente</span><a href='evento_singolo.php?nome_evento={$eventoLink}'><p class='marked'>$nomeEvento</p></a></td>";
+                if (!($tmp && $giornoCorrente == $oggiG)) {
+                    $calendarioHTML .= "<td data-title='$giornoSettimanaC'><span class='eventoPresente'>$giornoCorrente</span><a href='evento_singolo.php?nome_evento={$eventoLink}'><p class='marked'>$nomeEvento</p></a></td>";
                 } else {
-                    $calendarioHTML .= "<td><span class='eventoPresenteOggi'>$giornoCorrente</span><a href='evento_singolo.php?nome_evento={$eventoLink}'><p class='marked'>$nomeEvento</p></a></td>";
+                    $calendarioHTML .= "<td data-title='$giornoSettimanaC'><span class='eventoPresenteOggi'>$giornoCorrente</span><a href='evento_singolo.php?nome_evento={$eventoLink}'><p class='marked'>$nomeEvento</p></a></td>";
                 }
                 $eventoStampato = true;
-
             }
         }
-    
     }
 
     if (!$eventoStampato) {
-        if(!($tmp && $giornoCorrente == $oggiG)){
-            $calendarioHTML .= "<td>$giornoCorrente</td>";
+        if (!($tmp && $giornoCorrente == $oggiG)) {
+            $calendarioHTML .= "<td data-title='$giornoSettimanaC'>$giornoCorrente</td>";
         } else {
-            $calendarioHTML .= "<td><span class='eventoOggi'>$giornoCorrente</span></td>";
+            $calendarioHTML .= "<td data-title='$giornoSettimanaC'><span class='eventoOggi'>$giornoCorrente</span></td>";
         }
     }
 
@@ -121,27 +121,31 @@ while ($giornoCorrente <= $numeroGiorni) {
     }
 
     $giornoCorrente++;
+    $indiceGiornoSettimana++;
 }
 
+// Giorni vuoti finali
 $giornoSuccessivo = 1;
 for ($i = 0; $i < $giorniFinaliVuoti; $i++) {
-    $calendarioHTML .= "<td class='giornoFuori'>$giornoSuccessivo</td>";
+    $giornoSettimana = $giorniSettimana[$indiceGiornoSettimana % 7];
+    $calendarioHTML .= "<td class='giornoFuori' data-title='$giornoSettimana'>$giornoSuccessivo</td>";
     $giornoSuccessivo++;
+    $indiceGiornoSettimana++;
 }
 
 $calendarioHTML .= "</tr>";
 $calendarioHTML .= "</tbody>";
 
+// Selezione mese
 for ($i = 1; $i <= 12; $i++) {
     $paginaHTML->aggiungiContenuto("[selected_mese_$i]", ($i == $mese) ? "selected" : "");
 }
 
+// Selezione anno
 for ($i = 2023; $i <= 2030; $i++) {
     $paginaHTML->aggiungiContenuto("[selected_anno_$i]", ($i == $anno) ? "selected" : "");
 }
 
-$paginaHTML->aggiungiContenuto("[calendario]",$calendarioHTML);
+$paginaHTML->aggiungiContenuto("[calendario]", $calendarioHTML);
 $paginaHTML->getPagina();
-
 ?>
-
